5ec2c81eddd1354ebb6b39d54dfd9ff6c7e03f7f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-02-15 20:23:33 +0600

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import CounterView from './features/Counter/CounterView';
+import PostView from './features/Posts/PostView';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-2xl'>Welcome to React-Redux</h1>
       <CounterView />
+      <PostView />
     </div>
   );
 }
--- src/app/Store.js
+++ src/app/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "../features/Counter/CounterSlice";
+import PostReducer from "../features/Posts/PostSlice";
 
 const store = configureStore({
     reducer: {
-        counter: CounterReducer
+        counter: CounterReducer,
+        posts: PostReducer,
     }
 })
 export default store
